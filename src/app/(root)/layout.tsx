4ddd97173497745d69d1f1448b02fc43a4c8d02e
Alberto Rodrigues <allio.professional@gmail.com>
2024-05-31 12:27:41 -0300

    chore: adding StreamVideoProvider

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,14 @@
 import React, { ReactNode } from "react";
+import StreamVideoProvider from "../../../providers/StremClientProvider";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  );
 };
 
 export default RootLayout;
