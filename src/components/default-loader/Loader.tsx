ec99d2871621bf886ed8ad4b606bb2961da0931b
Alberto Rodrigues <allio.professional@gmail.com>
2024-05-31 12:28:06 -0300

    feat: creating component loader

---
+++ src/components/default-loader/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { BiLoader, BiLoaderCircle } from 'react-icons/bi'
+
+export default function Loader(){
+  return (
+    <div className='flex self-center h-screen w-full'>
+        <BiLoaderCircle  className='text-3xl'/>
+      
+    </div>
+  )
+}
