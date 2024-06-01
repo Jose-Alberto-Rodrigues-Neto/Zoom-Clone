254367006a4d486ad4bcf15e6744668e324877fa
Alberto Rodrigues <allio.professional@gmail.com>
2024-06-01 18:30:29 -0300

    feat: creating meeting room

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import '@stream-io/video-react-sdk/dist/css/styles.css';
 import { ClerkProvider } from "@clerk/nextjs";
-
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
